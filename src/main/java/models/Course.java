402d46c1ad6c3ceab530b31f87760b659c6c56b1
StuartCharters <charters.stuart@gmail.com>
2018-07-11 15:32:41 +0100

    set up course with getters/setters

--- src/main/java/models/Course.java
+++ src/main/java/models/Course.java
@@ -1,4 +1,40 @@
 package models;
 
 public class Course {
+
+    private int id;
+    private String name;
+    private String level;
+
+    public Course(){}
+
+    public Course(String name, String level){
+        this.name = name;
+        this.level = level;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setId(){
+        this.id = id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getLevel(){
+        return this.level;
+    }
+
+    public void setLevel(String level){
+        this.level = level;
+    }
+
 }
