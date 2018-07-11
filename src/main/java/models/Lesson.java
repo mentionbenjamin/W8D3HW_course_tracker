054baf0c9a204eeb458d50a7c81d59e4d6929c30
StuartCharters <charters.stuart@gmail.com>
2018-07-11 15:35:20 +0100

    set up lesson with getters/setters

--- src/main/java/models/Lesson.java
+++ src/main/java/models/Lesson.java
@@ -1,4 +1,40 @@
 package models;
 
 public class Lesson {
+
+    private int id;
+    private String title;
+    private int classroomNumber;
+
+    public Lesson(){}
+
+    public Lesson(String title, int classroomNumber){
+        this.title = title;
+        this.classroomNumber = classroomNumber;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setId(){
+        this.id = id;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public int getClassroomNumber(){
+        return this.classroomNumber;
+    }
+
+    public void setClassroomNumber(int classroomNumber){
+        this.classroomNumber = classroomNumber;
+    }
+
 }
