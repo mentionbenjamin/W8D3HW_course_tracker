6a6d4f5df842d26eb08488219307613086cc2e03
StuartCharters <charters.stuart@gmail.com>
2018-07-11 16:08:19 +0100

    added save methods to runner

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,4 +1,10 @@
 
+import db.DBHelper;
+import models.Course;
+import models.Lesson;
+import models.Mentor;
+import models.Student;
+
 import java.util.List;
 
 public class Runner {
@@ -6,6 +12,21 @@ public class Runner {
     public static void main(String[] args) {
 
 
+        Lesson lesson1 = new Lesson("Computer Science", 314);
+        DBHelper.save(lesson1);
+        Lesson lesson2 = new Lesson("History", 1066);
+        DBHelper.save(lesson2);
+
+        Student student1 = new Student("Stuart", 26, 42);
+        DBHelper.save(student1);
+        Student student2 = new Student("Ben", 24, 23);
+        DBHelper.save(student2);
+
+        Course course = new Course("Historic Computing", "BComp");
+        DBHelper.save(course);
+
+        Mentor mentor = new Mentor("Zsolt");
+        DBHelper.save(mentor);
 
         System.exit(0);
     }
